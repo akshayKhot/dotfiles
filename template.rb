0ecefd64043229b25a9029dcf6cec6353152884a
Akshay Khot <akshay.khot@hey.com>
2023-04-18 18:49:12 -0700

    add rails

---
+++ template.rb
@@ -0,0 +1,14 @@
+gem 'lograge'
+
+gem_group :development, :test do
+  gem 'awesome_print'
+  gem 'dotenv-rails'
+  gem 'factory_bot_rails'
+  gem 'minitest'
+end
+
+gem_group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'annotate'
+end
